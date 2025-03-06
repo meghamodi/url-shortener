8c9e34568be79251772c0f1cfb140bcc362a4cce
Megha Modi <meghamodi007@gmail.com>
2025-03-05 19:23:27 -0800

    extract url from the client

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 // long/original url is hashed to make it a small url
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
-app.get('/user',(req,res)=>{
+app.post('/shortenUrl',(req,res)=>{
+    var client_url = req.body.longUrl;
+    console.log(client_url)
+    
     res.send("Hellow World")
 
 })
 app.listen(3000,()=>{
+   
     console.log("This is working as expected")
 })
