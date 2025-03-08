54f8b651a4b7449885ca17bada2571d1b4d42c73
Megha Modi <meghamodi007@gmail.com>
2025-03-07 17:25:23 -0800

    storing url,hash in hashmap

---
+++ models/storage.js
@@ -0,0 +1,13 @@
+const URL_hash_Mapping = new Map();
+
+function storeURL(clientURL,hash){
+    URL_hash_Mapping.set(clientURL,hash)
+}
+
+function generateHash(hash){
+    return 
+
+}
+module.exports={
+    storeURL,generateHash
+}
