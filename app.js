475ff6fc2ad20802b5c02b9a85f1addc6cc207d2
Megha Modi <meghamodi007@gmail.com>
2025-03-08 11:52:28 -0800

    logic to add hash when url is not present in mapping

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 var tools = require('./utils/extractUrl.js');
+var storage = require('./models/storage.js');
 const { check, validationResult } = require('express-validator');
 
 
 app.post('/shortenUrl', [
     // Validate 'longUrl' directly in the middleware
-    check('longurl').isURL().withMessage('Invalid URL format')
+    check('longUrl').isURL().withMessage('Invalid URL format')
 ], (req, res) => {
     // Handle validation errors
     const errors = validationResult(req);
@@ -20,10 +21,15 @@ app.post('/shortenUrl', [
 
     // Extract the URL using the extractUrl function
     const clientUrl = tools.extractUrl(req);  // This will return the longUrl from the request body
+  
+    storage.storeURL(clientUrl)
+    const hashGenerator = storage.URL_hash_Mapping.get(clientUrl);
+    if (!hashGenerator){
+        return res.status(500).json({error:"hash not found"})
 
-    // Your URL shortening logic goes here
-    // For now, we will simply send back the original URL as a "shortened" example
-    res.send(`Shortened URL: ${clientUrl}`);
+    }
+
+    res.json({shortenedUrl: `http://localhost.com:3000/${hashGenerator}`})
 });
 app.listen(3000,()=>{
    
--- models/storage.js
+++ models/storage.js
@@ -1,13 +1,18 @@
 const URL_hash_Mapping = new Map();
 
-function storeURL(clientURL,hash){
-    URL_hash_Mapping.set(clientURL,hash)
+function storeURL(clientURL){
+    if (!URL_hash_Mapping.has(clientURL)){
+        const hash = generateHash()
+        URL_hash_Mapping.set(clientURL,hash)
+    }
+    console.log(URL_hash_Mapping)
 }
 
-function generateHash(hash){
-    return 
-
+function generateHash(){
+    return Math.random().toString(36).slice(2,6)
 }
+
+
 module.exports={
-    storeURL,generateHash
+    storeURL,generateHash,URL_hash_Mapping
 }
